5303e64b2ae9ff2c344577a2e44dbff50b67c41b
Chad Thompson <Chad.Thompson@Qorvo.com>
2022-01-10 18:55:56 -0500

    believe it works

--- static/js/app.js
+++ static/js/app.js
@@ -60,11 +60,13 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    
+    Object.entries(filters).forEach(([key,value])=>{
+      filteredData = filteredData.filter(row => row[key]=== value);
+    });
   
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
-  }
+  };
   
   // 2. Attach an event to listen for changes to each filter
   d3.selectAll("input").on("change", updateFilters);
